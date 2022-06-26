aeff6431a5f78eb8655ff067aeace85f88bfef4e
santiago.moncada <santiago.moncada@rappi.com>
2022-06-26 11:57:11 -0500

    host updated

--- core/configAPP.php
+++ core/configAPP.php
@@ -1,14 +1,14 @@
 <?php
 
-    const SERVER = "localhost";
-    const DB = "packapps";
-    const USER = "root";
-    const PASS = "";
+    // const SERVER = "localhost";
+    // const DB = "packapps";
+    // const USER = "root";
+    // const PASS = "";
 
-    // const SERVER = "sql309.vzpla.net";
-    // const DB = "user_23952903_bookverse";
-    // const USER = "user_23952903";
-    // const PASS = "santiago123";
+    const SERVER = "https://bookverse25.herokuapp.com/";
+    const DB = "ik1eybdutgxsm0lo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    const USER = "l662rn3oscw733ih";
+    const PASS = "icvfn049zrj6y0q0";
 
     const SGBD = "mysql:host=" . SERVER . ";dbname=" . DB;
 
